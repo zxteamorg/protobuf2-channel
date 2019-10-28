426bf03d8324a04693f5447211ed6be46e64c124
Maxim Anurin <m.anurin@maximanurin.local>
2019-10-29 01:42:27 +0200

    Initial commit

---
+++ binding/node/src/index.ts
@@ -0,0 +1,5 @@
+export { InvokeRequest } from "./generated/InvokeRequest_pb";
+export { InvokeResponse } from "./generated/InvokeResponse_pb";
+export { Message } from "./generated/Message_pb";
+export { Notification } from "./generated/Notification_pb";
+export { TypeMap } from "./generated/Type_pb";
