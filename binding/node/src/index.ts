c91035cd7ca6cf759d9cedeba8d01d716262f324
Maxim Anurin <m.anurin@maximanurin.local>
2019-11-08 19:24:32 +0200

    Add missing export InvokeResponseSuccess, InvokeResponseFailure

--- binding/node/src/index.ts
+++ binding/node/src/index.ts
@@ -1,5 +1,5 @@
 export { InvokeRequest } from "./generated/InvokeRequest_pb";
-export { InvokeResponse } from "./generated/InvokeResponse_pb";
+export { InvokeResponse, InvokeResponseSuccess, InvokeResponseFailure } from "./generated/InvokeResponse_pb";
 export { Message } from "./generated/Message_pb";
 export { Notification } from "./generated/Notification_pb";
 export { Type, TypeMap } from "./generated/Type_pb";
